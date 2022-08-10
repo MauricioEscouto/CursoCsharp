2d35e6bc075123eef22af34195a2f6775b70195e
Mauricio Escouto Melo <103608018+MauricioEscouto@users.noreply.github.com>
2022-08-10 14:07:27 -0300

    Add files via upload

---
+++ Explorando_a_linguagem/3-ConversãoVariavéis/3-ConversãoVariavéis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double salario;
+        salario = 1212.20;
+        Console.WriteLine(salario);
+
+        int salarioInteiro;
+        salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro);
+
+        float altura = 1.6576666666666666f;
+        Console.WriteLine(altura);
+
+    }
+}
---
+++ Explorando_a_linguagem/5-CaracteresETexto/5-CaracteresETexto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Caracteres e textos");
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+        Console.ReadLine();
+    }
+}
