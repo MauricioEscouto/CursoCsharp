cee3fc368b2d104e971e64effd860344f285f56d
Mauricio Escouto <mauriciocunha9953@gmail.com>
2022-08-16 13:51:42 -0300

    primeiros passos do projeto bytebank

---
+++ OrientacaoObjeto/byteBank/byteBank/ContaCorrente.cs
@@ -0,0 +1,13 @@
+﻿namespace byteBank
+{
+    class contaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int numero_agencia;
+        public string nome_agencia;
+        public double saldo;
+    }
+
+
+}
--- OrientacaoObjeto/byteBank/byteBank/Program.cs
+++ OrientacaoObjeto/byteBank/byteBank/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using byteBank;
 
-namespace byteBank
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Console.WriteLine("Boas-vindas ao banco ByteBank\n");
+
+contaCorrente conta1 = new contaCorrente();
+conta1.titular = "André Silva";
+conta1.conta = "10123-X";
+conta1.numero_agencia = 23;
+conta1.nome_agencia = "Agência Central";
+conta1.saldo = 100;
+
+contaCorrente conta2 = new contaCorrente();
+conta2.titular = "Amanda Nogueira";
+conta2.conta = "10106-X";
+conta2.numero_agencia = 321;
+conta2.nome_agencia = "Agência Central";
+conta2.saldo = 100;
+
+Console.WriteLine("Titular: " + conta1.titular);
+Console.WriteLine("Conta: " + conta1.conta);
+Console.WriteLine("Número Agencia: " + conta1.numero_agencia);
+Console.WriteLine("Nome da Agencia: " + conta1.nome_agencia);
+Console.WriteLine("Saldo: " + conta1.saldo);
+
+
+
