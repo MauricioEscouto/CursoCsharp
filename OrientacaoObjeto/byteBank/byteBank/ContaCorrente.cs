461c6460c36f9384758b7b84aedee4bfd1b91f2f
Mauricio Escouto <mauriciocunha9953@gmail.com>
2022-08-17 18:00:58 -0300

    continuando com o curso de Csharp

--- OrientacaoObjeto/byteBank/byteBank/ContaCorrente.cs
+++ OrientacaoObjeto/byteBank/byteBank/ContaCorrente.cs
@@ -1,13 +1,53 @@
-﻿namespace byteBank
+﻿
+namespace byteBank
 {
-    class contaCorrente
+    class ContaCorrente
     {
-        public string titular;
+        public Cliente titular;
         public string conta;
         public int numero_agencia;
         public string nome_agencia;
         public double saldo;
-    }
 
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo = saldo + valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
+    }
 
 }
--- OrientacaoObjeto/byteBank/byteBank/Program.cs
+++ OrientacaoObjeto/byteBank/byteBank/Program.cs
@@ -3,25 +3,38 @@ using byteBank;
 
 Console.WriteLine("Boas-vindas ao banco ByteBank\n");
 
-contaCorrente conta1 = new contaCorrente();
-conta1.titular = "André Silva";
+ContaCorrente conta1 = new ContaCorrente();
 conta1.conta = "10123-X";
 conta1.numero_agencia = 23;
 conta1.nome_agencia = "Agência Central";
 conta1.saldo = 100;
 
-contaCorrente conta2 = new contaCorrente();
-conta2.titular = "Amanda Nogueira";
+ContaCorrente conta2 = new ContaCorrente();
 conta2.conta = "10106-X";
 conta2.numero_agencia = 321;
 conta2.nome_agencia = "Agência Central";
 conta2.saldo = 100;
 
-Console.WriteLine("Titular: " + conta1.titular);
-Console.WriteLine("Conta: " + conta1.conta);
-Console.WriteLine("Número Agencia: " + conta1.numero_agencia);
-Console.WriteLine("Nome da Agencia: " + conta1.nome_agencia);
-Console.WriteLine("Saldo: " + conta1.saldo);
+//Console.WriteLine("Titular: " + conta1.titular);
+//Console.WriteLine("Conta: " + conta1.conta);
+//Console.WriteLine("Número Agencia: " + conta1.numero_agencia);
+//Console.WriteLine("Nome da Agencia: " + conta1.nome_agencia);
+//Console.WriteLine("Saldo: " + conta1.saldo);
+
+
+//Console.WriteLine("Saldo da Amanda pré-transferência: " + conta2.saldo);
+//Console.WriteLine("Saldo do André  pré- Transferência: " + conta1.saldo);
+//conta1.Transferir(50, conta2);
+//Console.WriteLine("Saldo da Amanda pós-transferência: " + conta2.saldo);
+//Console.WriteLine("Saldo do André pós-transferência: " + conta1.saldo);
+
+Cliente cliente = new Cliente();
+cliente.nome = "Andre Silva";
+cliente.cpf = "10253541263";
+cliente.profissao = "Programador C#";
+
+ContaCorrente conta3 = new ContaCorrente();
+conta3.titular = cliente;
 
 
 
